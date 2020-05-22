8910da404369002a321b41f50c5fe0c8bf1e67fb
Julian Moraes <julian_moraes@Julians-MacBook-Pro.local>
2020-05-22 16:08:07 -0400

    Updates

---
+++ precache-manifest.77bbaf372cffcb6fd754190dcbb0c264.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabc5f22c30acceb3d1fb9facb324043",
+    "url": "/gather/index.html"
+  },
+  {
+    "revision": "81c1a9c81ae26fb11aa7",
+    "url": "/gather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52932c51b57ee82e5cc6",
+    "url": "/gather/static/js/2.ab9317fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gather/static/js/2.ab9317fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c1a9c81ae26fb11aa7",
+    "url": "/gather/static/js/main.b6ec1022.chunk.js"
+  },
+  {
+    "revision": "f8ac951814e0f66731c0",
+    "url": "/gather/static/js/runtime-main.2243f66a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gather/static/media/logo.5d5d9eef.svg"
+  }
+]);
